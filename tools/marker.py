ce071d3eb00fbea958d9d2ab6c48886f90fe5f0c
Nick Manna <manna422@gmail.com>
2015-05-23 13:47:13 -0700

    marker tool: keypresses and file export added

--- tools/marker.py
+++ tools/marker.py
@@ -10,30 +10,48 @@ class MediaPlayer(QWidget):
         self.setGeometry(300, 300, 600, 200)
         self.media_obj = Phonon.MediaObject(self)
         self.timestamp = '00:00.000'
+        self.time_ms = 0
         self.playing = False
         self.mdata = []
         self.init_ts()
         self.set_buttons()
+        self.installEventFilter(self)
         self.open_file()
+
+    def eventFilter(self, widget, event):
+        if event.type() == QEvent.KeyPress:
+            key = event.key()
+            if key == Qt.Key_Space:
+                self.play_pause()
+            elif key == Qt.Key_W:
+                self.set_mark_a()
+            elif key == Qt.Key_A:
+                self.set_mark_b()
+            elif key == Qt.Key_S:
+                self.set_mark_c()
+            elif key == Qt.Key_D:
+                self.set_mark_d()
+            return True
+        return QWidget.eventFilter(self, widget, event)
 
 
     def set_buttons(self):
         self.playButton = QPushButton('Pause', self)
-        markAButton = QPushButton('A', self)
-        markBButton = QPushButton('B', self)
-        markCButton = QPushButton('C', self)
-        markDButton = QPushButton('D', self)
+        self.markAButton = QPushButton('A', self)
+        self.markBButton = QPushButton('B', self)
+        self.markCButton = QPushButton('C', self)
+        self.markDButton = QPushButton('D', self)
 
-        markAButton.move(100, 0)
-        markBButton.move(200, 0)
-        markCButton.move(300, 0)
-        markDButton.move(400, 0)
+        self.markAButton.move(100, 0)
+        self.markBButton.move(200, 0)
+        self.markCButton.move(300, 0)
+        self.markDButton.move(400, 0)
 
         self.playButton.clicked.connect(self.play_pause)
-        markAButton.clicked.connect(self.set_mark_a)
-        markBButton.clicked.connect(self.set_mark_b)
-        markCButton.clicked.connect(self.set_mark_c)
-        markDButton.clicked.connect(self.set_mark_d)
+        self.markAButton.clicked.connect(self.set_mark_a)
+        self.markBButton.clicked.connect(self.set_mark_b)
+        self.markCButton.clicked.connect(self.set_mark_c)
+        self.markDButton.clicked.connect(self.set_mark_d)
 
         self.seeker = QSlider(Qt.Orientation.Horizontal, self)
         self.seeker.setValue(0)
@@ -66,6 +84,7 @@ class MediaPlayer(QWidget):
         self.timeDisplay.display(self.timestamp)
 
     def update_ts(self, time_ms):
+        self.time_ms = time_ms
         self.timestamp = str(time_ms  % 1000)
         time_ms /= 1000
         self.timestamp = str(time_ms % 60) + '.' + self.timestamp
@@ -74,7 +93,7 @@ class MediaPlayer(QWidget):
         self.timeDisplay.display(self.timestamp)
 
     def set_mark(self, mark):
-        self.mdata.append((self.timestamp, mark))
+        self.mdata.append((int(self.time_ms), mark))
         print 'mark hit %s' % mark
 
     def set_mark_a(self):
@@ -100,18 +119,24 @@ class MediaPlayer(QWidget):
     def open_file(self):
         dialog = QFileDialog()
         dialog.setViewMode(QFileDialog.Detail)
-        filename = dialog.getOpenFileName(self,
+        self.filename = dialog.getOpenFileName(self,
              'Open audio file', '/home',
              "Audio Files (*.mp3 *.wav *.ogg)")[0]
         self.audio_output = Phonon.AudioOutput(Phonon.MusicCategory, self)
         Phonon.createPath(self.media_obj, self.audio_output)
-        self.media_obj.setCurrentSource(Phonon.MediaSource(filename))
+        self.media_obj.setCurrentSource(Phonon.MediaSource(self.filename))
         self.media_obj.tick.connect(self._time_change)
         self.media_obj.totalTimeChanged.connect(self._total_time_change)
         self.media_obj.play()
         self.playButton.setText("Pause")
         self.seeker.setEnabled(True)
         self.playing = True
+
+    def store_mdata(self):
+        f = open((self.filename + '.mdata'), 'w')
+        for inst in self.mdata:
+            f.write('%s %s\n' % inst)
+        f.close()
 
 
 if __name__ == '__main__':
@@ -120,7 +145,8 @@ if __name__ == '__main__':
         myWindow = MediaPlayer()
         myWindow.show()
         myApp.exec_()
-        print myWindow.mdata, '\n'
+        myWindow.mdata.sort(key=lambda tup: tup[0])
+        myWindow.store_mdata()
         sys.exit(0)
     except Exception, e:
         print 'Error', e
